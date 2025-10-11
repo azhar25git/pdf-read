41847a386516e300d3211e2aa6edc71b38db6644
Azhar Uddin <16219627+azhar25git@users.noreply.github.com>
2025-10-11 22:47:24 +0600

    finalize TransalliancePdfAssistant

--- app/Assistants/TransalliancePdfAssistant.php
+++ app/Assistants/TransalliancePdfAssistant.php
@@ -2,6 +2,7 @@
 
 namespace App\Assistants;
 
+use App\GeonamesCountry;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
 
@@ -11,16 +12,14 @@ class TransalliancePdfAssistant extends PdfClient
 
     protected int $index = 0;
 
-    protected string $datePattern = '/^(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/[0-9]{4}$/';
+    protected string $datePattern = '/^(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/[0-9]{2}$/';
 
     protected string $timePattern = '/^(2[0-3]|[0-1]?[0-9]):([0-5]?[0-9]|[0-5][0-9]):([0-5]?[0-9]|[0-5][0-9])$/';
 
-    protected string $timeRangePattern = '^(?:(?:[0-1]?[0-9]|2[0-3])h[0-5][0-9])\s*-\s*(?:(?:[0-1]?[0-9]|2[0-3])h[0-5][0-9])$';
-
-    protected string $dateFormat = 'd/m/Y';
+    protected string $timeRangePattern = '/^(?:(?:[0-1]?[0-9]|2[0-3])h[0-5][0-9])\s*-\s*(?:(?:[0-1]?[0-9]|2[0-3])h[0-5][0-9])$/';
 
     protected array $roadNames = [
-        'road', 'way', 'chem.', 'lane', 'rue', 'street', 'rugiu', 'suite', 'blvd', 'RD'
+        'road', 'way', 'chem.', 'lane', 'rue', 'street', 'rugiu', 'suite', 'blvd', 'RD',
     ];
 
     public static function validateFormat(array $lines)
@@ -41,7 +40,7 @@ class TransalliancePdfAssistant extends PdfClient
 
         $date_from_line = $this->findLine('Date/Time :', exactMatch: true);
         $time_from_line = $this->findDate($this->timePattern);
-        $datetime_from = Carbon::createFromFormat($this->dateFormat, $this->lines[$date_from_line + 1])->startOfDay();
+        $datetime_from = Carbon::createFromFormat('d/m/Y', $this->lines[$date_from_line + 1])->startOfDay();
         $datetime_from = $datetime_from->setTimeFromTimeString($time_from_line)->toISOString();
 
         $customer = [
@@ -61,72 +60,102 @@ class TransalliancePdfAssistant extends PdfClient
             ],
         ];
 
-        [$loading_locations, $cargos] = $this->extractLoadingLocations($this->findAllLines('Loading', exactMatch: true));
-        dd($loading_locations, $cargos);
+        $loading_locations = $this->extractLocations($this->findAllLines('Loading', exactMatch: true));
+        $cargos = $this->extractCargo($this->findAllLines('Loading', exactMatch: true));
 
-        // $clearance_locations = $this->extractDestinationLocations($this->findAllLines('Clearance', exactMatch: true));
+        $destination_locations = $this->extractLocations($this->findAllLines('Delivery', exactMatch: true));
 
-        // foreach ($clearance_locations as &$loc) {
-        //     $loc['company_address']['comment'] = 'clearance';
-        // }
+        $order_reference_line = $this->findLine('REF.:');
+        $order_reference = trim(Str::replace('REF.:', '', $this->lines[$order_reference_line]));
 
-        // $delivery_locations = $this->extractDestinationLocations($this->findAllLines('Delivery', exactMatch: true));
+        $freight_price_line = $this->findLine('SHIPPING PRICE');
+        $freight_price = uncomma($this->lines[$freight_price_line + 1]);
+        $freight_currency = 'EUR';
 
-        // foreach ($delivery_locations as &$loc) {
-        //     $loc['company_address']['comment'] = 'delivery';
-        // }
-        // $destination_locations = array_merge($clearance_locations, $delivery_locations);
+        $telephone_line = $this->findLine('Tel');
+        $customer_number = ltrim($this->lines[$telephone_line + 1], ': ');
 
-        // $order_reference_line = $this->findLine('Ziegler Ref');
-        // $order_reference = $this->lines[$order_reference_line + 1];
+        $data = compact(
+            'attachment_filenames',
+            'customer',
+            'loading_locations',
+            'destination_locations',
+            'cargos',
+            'order_reference',
+            'freight_price',
+            'freight_currency',
+            'customer_number'
+        );
 
-        // $telephone_line = $this->findLine('Telephone: ');
-        // $customer_number = ltrim($this->lines[$telephone_line], 'Telephone: ');
-
-        // $freight_price_line = $this->findLine('Rate');
-        // $freight_price = uncomma(preg_replace('/[^0-9.]/', '', $this->lines[$freight_price_line + 1]));
-        // $freight_currency = 'EUR';
-
-        // $data = compact(
-        //     'attachment_filenames',
-        //     'customer',
-        //     'loading_locations',
-        //     'destination_locations',
-        //     'cargos',
-        //     'order_reference',
-        //     'freight_price',
-        //     'freight_currency'
-        // );
-
-        // $this->createOrder($data);
+        $this->createOrder($data);
     }
 
-    protected function extractLoadingLocations(array $loading_locations_lines): array
+    protected function extractCargo(array $loading_locations_lines): array
     {
-        $locationResults = [];
         $cargoResults = [];
 
         foreach ($loading_locations_lines as $i => $linesIndex) {
-            $address_line = $this->findLine("ON:", startFrom:$linesIndex, exactMatch:true);
-            $contact_line = $this->findLine("Contact:", startFrom:$linesIndex, exactMatch:true);
-           
-            [$postal, $city] = $this->extractPostalAndCity($this->lines[$contact_line - 1]);
-            
-            $locationResults[$i]['company_address'] = [
-                'company' => $this->lines[$address_line + 1],
-                'street_address' => $this->lines[$address_line + 2],
-                'city' => $city,
-                'postal_code' => $postal,
-                'country' => Str::substr($postal, 0, 2),
-            ];
+            $cargo = [];
+            $cargo_line = $this->findLine('OT :', $linesIndex + 1);
+            if ($this->lines[$cargo_line + 2] == 'OT :') {
+                $cargo_line = $cargo_line + 2;
+            }
 
-            $date_line = $this->findDate($this->datePattern, $linesIndex);
-            $carbonDate = Carbon::createFromFormat($this->dateFormat, $this->lines[$date_line]);
+            $cargo['ldm'] = uncomma(Str::replace(' ', '', $this->lines[$cargo_line + 2]));
+            $cargo['weight'] = uncomma(Str::replace(' ', '', $this->lines[$cargo_line + 3]));
+            $cargo['package_type'] = 'other';
 
-            $timerange_line = $this->findDate($this->timeRangePattern, $linesIndex, $linesIndex+13);
+            $cargoResults[$i] = $cargo;
+        }
 
-            if($timerange_line) {
-                $times = explode('-', $this->lines[$timerange_line]);
+        return $cargoResults;
+    }
+
+    protected function extractLocations(array $loading_locations_lines): array
+    {
+        $locationResults = [];
+
+        foreach ($loading_locations_lines as $i => $linesIndex) {
+            $address_line = $this->findLine('ON:', startFrom: $linesIndex, exactMatch: true);
+            $contact_line = $this->findLine('Contact:', startFrom: $linesIndex, exactMatch: true);
+
+            $company = $this->lines[$address_line + 1];
+
+            foreach ($this->roadNames as $name) {
+                if ($street_address_line = $this->findLine($name, $address_line + 2, $contact_line, exactMatch: false)) {
+                    break;
+                }
+            }
+
+            $street_address = $this->lines[$street_address_line] ?? $this->lines[$contact_line - 2];
+            [$postal_code, $city] = $this->extractPostalAndCity($this->lines[$contact_line - 1]);
+
+            $country_name_arr = explode(' ', $company);
+            $country_name = ucwords(strtolower($country_name_arr[max(count($country_name_arr) - 1, 0)] ?? ''));
+            $country = GeonamesCountry::getIso($country_name) ?? null;
+
+            $locationResults[$i]['company_address'] = compact(
+                'company',
+                'street_address',
+                'city',
+                'postal_code'
+            );
+
+            if ($country) {
+                $locationResults[$i]['company_address']['country'] = $country;
+            }
+
+            $date_line = $this->findDate($this->datePattern, $linesIndex + 1);
+
+            $carbonDate = Carbon::createFromFormat('d/m/y', $this->lines[$date_line]);
+            $datetime_from = $carbonDate->startOfDay()->toISOString();
+            $datetime_to = null;
+
+            $timerange_line = $this->findDate($this->timeRangePattern, $linesIndex, $linesIndex + 13);
+
+            if ($timerange_line) {
+                $times = explode('-', Str::replace(' ', '', $this->lines[$timerange_line]));
+
                 $datetime_from = $carbonDate->startOfDay()
                     ->addHours($this->parseHour($times[0]))
                     ->toISOString();
@@ -135,72 +164,11 @@ class TransalliancePdfAssistant extends PdfClient
                     ->addHours($this->parseHour($times[1]))
                     ->toISOString();
             }
-            else {
-                $datetime_from = $carbonDate->startOfDay()->toISOString();
-                $datetime_to = '';
-            }
 
-            $locationResults[$i]['time'] = compact('datetime_from', 'datetime_to');
+            $locationResults[$i]['time']['datetime_from'] = $datetime_from;
 
-            $cargo = [];
-            $cargo_line = $this->findLine('pallets', $linesIndex + 1, $linesIndex + 9);  // max cargo lines 9
-
-            if (! $cargo_line) {
-                $cargo['package_count'] = 1;
-                $cargo_line = $this->findLine('PICK UP', $linesIndex + 1, $linesIndex + 9);
-                $cargo['package_type'] = 'other';
-            } else {
-                $cargo['package_count'] = (int) explode(' ', $this->lines[$cargo_line])[0];
-                $cargo['package_type'] = 'pallet';
-            }
-
-            $cargoResults[$i] = $cargo;
-        }
-
-        return [$locationResults, $cargoResults];
-    }
-
-    protected function extractDestinationLocations(array $destination_locations_lines): array
-    {
-        $locationResults = [];
-
-        foreach ($destination_locations_lines as $i => $linesIndex) {
-
-            $date_line = $this->findDate($this->datePattern, $linesIndex);
-
-            foreach ($this->roadNames as $name) {
-                if ($street_address_line = $this->findLine($name, $linesIndex + 1, $linesIndex + 9, exactMatch: false)) {
-                    break;
-                }
-            }
-
-            $city_postcode_line = ($this->findLine('Delivery', $linesIndex + 1, exactMatch: true) ?? $this->findLine('- Payment', $linesIndex + 1, exactMatch: false)) - 1;
-
-            $city_postcode_line = (stripos($this->lines[$city_postcode_line], 'pallets') !== false) ? $city_postcode_line - 1 : $city_postcode_line;
-
-            [$postal, $city] = $this->extractPostalAndCity($this->lines[$city_postcode_line]);
-
-            $locationResults[$i]['company_address'] = [
-                'company' => $this->lines[$linesIndex + 1],
-                'street_address' => $this->lines[$street_address_line],
-                'city' => Str::replace(',', '', $city),
-                'postal_code' => Str::replace(',', '', $postal),
-            ];
-
-            $timerange_line = $this->findLine('Time To:', $linesIndex);
-
-            if ($timerange_line) {
-                $carbonDate = Carbon::createFromFormat($this->dateFormat, $this->lines[$date_line]);
-
-                $times = explode('-', $this->lines[$timerange_line]);
-                if (count($times) < 2) {
-                    $times = explode('TimeTo', Str::replace([':', ' '], '', $this->lines[$timerange_line]));
-                }
-
-                $locationResults[$i]['time'] = [
-                    'datetime_from' => $carbonDate->startOfDay()->addHours($this->parseHour($times[0]))->toISOString(),
-                    'datetime_to' => $carbonDate->startOfDay()->addHours($this->parseHour($times[1]))->toISOString(),
-                ];
+            if ($datetime_to) {
+                $locationResults[$i]['time']['datetime_to'] = $datetime_to;
             }
         }
 
@@ -264,8 +232,8 @@ class TransalliancePdfAssistant extends PdfClient
         $hourString = (int) (Str::endsWith($hourString, '0')
             ? rtrim($hourString, '0')
             : Carbon::parse($hourString)->format('H'));
-            
-        return Str::endsWith($hourString, 'h') ? Str::replace('h', '', $hourString): $hourString;
+
+        return Str::endsWith($hourString, 'h') ? Str::replace('h', '', $hourString) : $hourString;
     }
 
     protected function extractPostalAndCity(string $address): array
@@ -278,7 +246,7 @@ class TransalliancePdfAssistant extends PdfClient
             // join them if there are 2 parts (like "SS5" + "4JL")
             $postal = strtoupper(implode(' ', $matches[0]));
             // remove postal from address → remaining part is city
-            $city = trim(str_ireplace($postal, '', $address));
+            $city = trim(trim(str_ireplace($postal, '', $address), '-'));
         } else {
             $postal = '';
             $city = strtoupper($address);
